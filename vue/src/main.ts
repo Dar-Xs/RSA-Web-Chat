d1555aa27ea1a2e2f781a2de70e19dcd30d08d27
Dr.Xiong <1393744283@qq.com>
2023-06-26 11:41:03 +0800

    config tailwindcss

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import './assets/tailwind.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ vue/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,ts,js}'],
+  theme: {
+    extend: {
+      colors: {
+        
+      }
+    }
+  },
+  plugins: [],
+}
